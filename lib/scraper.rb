00c0d9e27a99b55b432a7b3e546e43cd24761001
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-29 16:20:17 +0000

    initial scrape test

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,19 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    site = Nokogiri::HTML(open(index_url))
+    student_card = site.css('div.roster-cards-container')
+    student_card.each do |info|
+      stu = Student.new
+      stu.name = info.css('a div.card-text-container')
+      stu.location = info.css('a div.card-text-container')
+      stu.profile_url = info.css('a div.card-text-container')
+    end
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
