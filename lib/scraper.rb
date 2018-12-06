8c4dc1c894ea9cdd9d19277c57a994e9f4f172d9
kjgmez <kjgmez1@gmail.com>
2018-12-06 00:04:12 -0500

    scraper fixes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,4 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
@@ -5,17 +6,20 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     site = Nokogiri::HTML(open(index_url))
-    student_card = site.css('div.roster-cards-container')
+    student_card = site.css('div.roster-cards-container div.student-card')
     student_card.each do |info|
-      stu = Student.new
-      stu.name = info.css('a div.card-text-container')
-      stu.location = info.css('a div.card-text-container')
-      stu.profile_url = info.css('a div.card-text-container')
+      stu = {}
+      stu[:location] = info.css('a div.card-text-container p').text
+      stu[:name] = info.css('a div.card-text-container h4').text
+      stu[:profile_url] = info.css('a').attribute('href').value
+      Student.new(stu)
+      binding.pry
     end
-    binding.pry
+    #binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
+    site = Nokogiri::HTML(open(profile_url))
 
   end
 
